ceaa93093ad48bbb97bb85171eb4706fe0ef335f
pahosler <pahosler@gmail.com>
2020-09-20 06:41:55 -0500

    api headers

--- functions/lambda/tickets.js
+++ functions/lambda/tickets.js
@@ -20,8 +20,7 @@ exports.handler = (event, context, callback) => {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Headers': 'application/json',
+        'Access-Control-Allow-Headers': '*',
       },
       body: JSON.stringify(res),
     }).catch((e) => {
